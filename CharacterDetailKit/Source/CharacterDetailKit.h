0d947562633157f2f16770e73e154deb718bb5cd
Rafael Bartolome <rafa@positipp.com>
2020-03-22 12:08:34 +0100

    New framework for character detail feature

---
+++ CharacterDetailKit/Source/CharacterDetailKit.h
@@ -0,0 +1,19 @@
+//
+//  CharacterDetailKit.h
+//  CharacterDetailKit
+//
+//  Created by Rafael Bartolome on 22/03/2020.
+//  Copyright © 2020 Rafael Bartolome. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CharacterDetailKit.
+FOUNDATION_EXPORT double CharacterDetailKitVersionNumber;
+
+//! Project version string for CharacterDetailKit.
+FOUNDATION_EXPORT const unsigned char CharacterDetailKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CharacterDetailKit/PublicHeader.h>
+
+
