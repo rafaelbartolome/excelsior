764e64b3932c3ee1bf758657968de1a0838119f5
Rafael Bartolome <rafa@positipp.com>
2020-03-22 18:12:40 +0100

    Remove fake data

--- DataProviders/Source/Repository/Networking/CharacterService.swift
+++ DataProviders/Source/Repository/Networking/CharacterService.swift
@@ -47,42 +47,25 @@ extension CharacterService: CharacterServiceProtocol {
             case .failure(let error):
                 completion(.failure(CharacterRepositoryError(withResponseError: error)))
             }
-
         }
-//        #warning("TODO: WIP fake data")
-////        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-////            completion(.failure(.notFound))
-////        }
-//
-//        let c1 = Character(id: 1,
-//                           name: "Thor", bio: "Thor es guay",
-//                           thumbnailURL: URL(string: "https://i.annihil.us/u/prod/marvel/i/mg/7/30/5e67fba816991/portrait_uncanny.jpg")!,
-//                           modified: Date())
-//        let c2 = Character(id: 2,
-//                           name: "IronMan", bio: "Tambien es guay",
-//                           thumbnailURL: URL(string: "https://i.annihil.us/u/prod/marvel/i/mg/9/30/5e67aa73b0d81/portrait_uncanny.jpg")!,
-//                           modified: Date())
-//        let c3 = Character(id: 3,
-//                           name: "Super Lopez", bio: "Tambien es guay",
-//                           thumbnailURL: URL(string: "https://i.annihil.us/u/prod/marvel/i/mg/9/30/4/portrait_uncanny.jpg")!,
-//                           modified: Date())
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-//            completion(.success([c1, c2, c3]))
-//        }
-        
     }
     
     func character(with id: Int, completion: @escaping CharacterCompletion) {
-        #warning("TODO: WIP fake data")
-//                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-//                    completion(.failure(.notFound))
-//                }
-        let c1 = Character(id: 1,
-                           name: "Thor", bio: "Thor es guay",
-                           thumbnailURL: URL(string: "https://i.annihil.us/u/prod/marvel/i/mg/7/30/5e67fba816991/portrait_uncanny.jpg")!,
-                           modified: Date())
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            completion(.success(c1))
+        let request = GetCharacter(characterId: id)
+        apiClient.send(request) { response in
+            switch response {
+            case .success(let dataContainer):
+                let networkCharacters = dataContainer.results
+                guard let networkCharacter = networkCharacters.first else {
+                    completion(.failure(.notFound))
+                    return
+                }
+                let character = Character(withResponse: networkCharacter)
+                
+                completion(.success(character))
+            case .failure(let error):
+                completion(.failure(CharacterRepositoryError(withResponseError: error)))
+            }
         }
     }
 }
